fe70c9d4b4b7961569a6da23b3fde049ab1f3b72
Vitaliy Zarubin <keygenqt@pinxterapp.com>
2020-03-05 10:22:50 +0300

    fix font-awesome

--- FontAwesomeAsset.php
+++ FontAwesomeAsset.php
@@ -9,14 +9,8 @@ use \yii\web\AssetBundle;
  */
 class FontAwesomeAsset extends AssetBundle 
 {
-    public $sourcePath = '@bower/font-awesome'; 
-    public $css = [ 
-        'css/font-awesome.min.css', 
-    ];
-    public $publishOptions = [
-        'only' => [
-            'fonts/*',
-            'css/font-awesome.min.css',
-        ]
+    public $sourcePath = '@bower/font-awesome';
+    public $css = [
+        'css/all.css',
     ];
 }  
